68cfbe93fd1371cae603f21c3572c51235087238
Ananna1299 <anannapaul1299@gmail.com>
2025-11-10 19:04:16 +0600

    get course by id

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const cors=require("cors")
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
+console.log(process.env) 
 
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
-//onlineLearningDB
-//BmmexGv78Yw8ey18
 
-const uri = "mongodb+srv://onlineLearningDB:BmmexGv78Yw8ey18@cluster0.5w0kzva.mongodb.net/?appName=Cluster0";
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5w0kzva.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -65,6 +67,15 @@ async function run() {
       const filter_category = req.query.search
       const result = await coursesCollection.find({category: {$regex: filter_category, $options: "i"}}).toArray()
       res.send(result)
+    })
+
+     //get api(get a specific course)
+    app.get("/courses/:id",async(req,res)=>{
+        const id=req.params.id;
+        const query={_id: new ObjectId(id)}
+        const result = await coursesCollection.findOne(query);
+        //console.log(result)
+        res.send(result)
     })
 
 
